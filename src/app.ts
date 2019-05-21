0586c49eda461995b37832edbef078fe52f047ca
Joel Ross <joelross@uw.edu>
2019-05-21 07:59:10 -0700

    Add completed version

---
+++ src/app.ts
@@ -0,0 +1,74 @@
+import {TTTGame as Game, Player} from "./model";
+
+//for CLI interactiv
+const readline = require('readline');
+const io = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout
+});
+
+
+class Controller {
+  constructor(private game: Game, private view: View){}
+
+  start() {
+    this.view.display();
+    this.takeTurn();
+  }
+
+  takeTurn() {
+    this.view.showPrompt();
+    io.question('> ', (input: string) => {
+      try {
+        let cell = input.split(',');
+        let wasLegal = game.makeMove(Number(cell[0]), Number(cell[1]));
+        if(wasLegal){
+          this.view.display();
+          if(game.getWinner() !== Player[' ']){
+            this.view.showWinner(game.getWinner());
+            io.close()
+            return; //end
+          }
+        }
+      } catch(e) {} //for parsing errors
+
+      this.takeTurn(); //recurse!
+    })
+  }
+}
+
+class View {
+  constructor(private game: Game){}
+
+  //draw the game board
+  display() {
+    console.log("    0   1   2")
+    for(let i=0; i<this.game.size; i++) {
+      let row = i+"   ";
+      for(let j=0; j<this.game.size; j++) {
+        row += Player[this.game.getPiece(i,j)];
+        if(j < this.game.size - 1) 
+          row += " | ";
+      }
+      console.log(row);
+      if(i < this.game.size -1)
+        console.log("   -----------");
+    }
+    console.log("");
+  }
+
+  showPrompt() {
+    console.log(Player[game.getCurrentPlayer()]+"'s turn. Pick a spot [row, col]");
+  }
+
+  showWinner(winner: Player) {
+    console.log(Player[winner]+" is the winner!");
+  }
+}
+
+
+//run the program!
+let game:Game = new Game();
+let view:View = new View(game);
+let ctrl:Controller = new Controller(game, view);
+ctrl.start();
