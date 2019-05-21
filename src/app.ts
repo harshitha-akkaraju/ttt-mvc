ddc28dfb1e992aa01f64a5816b9b4b1860855721
Joel Ross <joelross@uw.edu>
2019-05-21 08:18:47 -0700

    Add starter code

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,95 @@
-import {TTTGame as Game, Player} from "./model";
-
-//for CLI interactiv
+//for CLI interactivity
 const readline = require('readline');
 const io = readline.createInterface({ 
   input: process.stdin, 
   output: process.stdout
 });
 
+//differentiates between players (Player[' '] is "none")
+export enum Player {X = 0, O = 1, ' ' = ' '}
 
-class Controller {
-  constructor(private game: Game, private view: View){}
+/**
+ * Represents a game of Tic Tac Toe.
+ * Board size is hard-coded at 3.
+ */
+export class TTTGame {
+  private gameBoard: Player[][] = [];
+  private currentPlayer: Player = Player.X;
+  private winner: Player = Player[' '];
+  public readonly size = 3; //hard-coded for simplicity
 
-  start() {
-    this.view.display();
+  constructor() {
+    this.resetBoard(); //initialize board
+  }
+
+  resetBoard() {
+    this.gameBoard = [
+      [Player[' '], Player[' '], Player[' ']],
+      [Player[' '], Player[' '], Player[' ']],
+      [Player[' '], Player[' '], Player[' ']],
+    ];
+  }
+
+  //returns if successful or not
+  makeMove(x: number, y: number): boolean { 
+    if(this.winner !== Player[' ']) return false; //don't move if won
+    if(x <0 || x > 2 || y < 0 || y > 2) return false; //out of bounds
+    if(this.gameBoard[x][y] !== Player[' ']) return false; //don't move if occupied
+
+    this.gameBoard[x][y] = this.currentPlayer; //make move
+
+    /*check if we now have a winner*/
+    let gb = this.gameBoard; //shortcut alias
+
+    //check row
+    if(gb[x][0] === gb[x][1] && gb[x][1] === gb[x][2]) this.winner = this.currentPlayer;
+    
+    //check col
+    if(gb[0][y] === gb[1][y] && gb[1][y] === gb[2][y]) this.winner = this.currentPlayer;
+
+     //check diag
+    if( gb[1][1] !== Player[' '] && (
+        (gb[0][0] === gb[1][1] && gb[1][1] === gb[2][2]) || 
+        (gb[2][0] === gb[1][1] && gb[1][1] === gb[0][2]) )) 
+      this.winner = this.currentPlayer
+
+    this.currentPlayer = (Number(this.currentPlayer)+1) % 2; //toggle
+
+    return true; //valid move
+  }
+
+  getPiece(x: number, y: number): Player {
+    if(x <0 || x > 2 || y < 0 || y > 2) return Player[' ']; //out of bounds
+    return this.gameBoard[x][y];
+  }
+
+  getBoard() {
+    return this.gameBoard;
+  }
+  
+  getCurrentPlayer(): Player {
+    return this.currentPlayer;
+  }
+
+  getWinner(): Player {
+    return this.winner;
+  }
+
+  play() {
+    this.printBoard();
     this.takeTurn();
   }
 
   takeTurn() {
-    this.view.showPrompt();
+    this.showPrompt();
     io.question('> ', (input: string) => {
       try {
         let cell = input.split(',');
         let wasLegal = game.makeMove(Number(cell[0]), Number(cell[1]));
         if(wasLegal){
-          this.view.display();
+          this.printBoard();
           if(game.getWinner() !== Player[' ']){
-            this.view.showWinner(game.getWinner());
+            this.showWinner(game.getWinner());
             io.close()
             return; //end
           }
@@ -35,23 +99,19 @@ class Controller {
       this.takeTurn(); //recurse!
     })
   }
-}
-
-class View {
-  constructor(private game: Game){}
 
   //draw the game board
-  display() {
+  printBoard() {
     console.log("    0   1   2")
-    for(let i=0; i<this.game.size; i++) {
+    for(let i=0; i<this.size; i++) {
       let row = i+"   ";
-      for(let j=0; j<this.game.size; j++) {
-        row += Player[this.game.getPiece(i,j)];
-        if(j < this.game.size - 1) 
+      for(let j=0; j<this.size; j++) {
+        row += Player[this.getPiece(i,j)];
+        if(j < this.size - 1) 
           row += " | ";
       }
       console.log(row);
-      if(i < this.game.size -1)
+      if(i < this.size -1)
         console.log("   -----------");
     }
     console.log("");
@@ -68,7 +128,5 @@ class View {
 
 
 //run the program!
-let game:Game = new Game();
-let view:View = new View(game);
-let ctrl:Controller = new Controller(game, view);
-ctrl.start();
+let game: TTTGame = new TTTGame();
+game.play();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import {TTTGame as Game, Player, Observer} from "./model";
+import {TTTGame as Game, Player} from "./app";
 
 class View {
-  constructor(private game: Game, private ctrl: Controller){}
+  constructor(private game: Game){}
 
   display() {
     console.log("displaying...");
@@ -21,7 +21,7 @@ class View {
     }
 
     //show winner, if any
-    let winner = game.getWinner();
+    let winner = this.game.getWinner();
     if(winner !== Player[' ']){
       this.showWinner(winner)
       $('button').attr('disabled','disabled'); //disable all the buttons
@@ -33,45 +33,14 @@ class View {
 
   //callback for clicking
   handleClick(row: number, col: number) {
-    this.ctrl.takeTurn(row, col); //tell controller what to do
+    // ???
   }
 
   showPrompt() {
-    $('#message').html('<p class="lead">'+Player[game.getCurrentPlayer()]+"'s turn. Pick a spot!"+'</p>')
+    $('#message').html('<p class="lead">'+Player[this.game.getCurrentPlayer()]+"'s turn. Pick a spot!"+'</p>')
   }
 
   showWinner(winner: number) {
     $('#message').html('<p class="lead">'+Player[winner]+" is the winner!"+'</p>')
   }
-
-  //observer
-  notify() {
-    this.display();
-  }
 }
-
-class Controller {
-  private view: View;
-  constructor(private game: Game){
-    this.view = new View(game, this);
-    this.game.register(this.view); //connect game and view
-    this.view.display(); //show the initial view
-  }
-
-  //when told what to do, pass it along to the game
-  takeTurn(row: number, col: number) {
-    if(this.game.getWinner() === Player[' '])
-      this.game.makeMove(row, col);
-  }
-}
-
-//run the program!
-let game: Game = new Game();
-
-// game.makeMove(0,0); //x //debugging
-// game.makeMove(0,1); //o
-// game.makeMove(1,0); //x
-// game.makeMove(1,1); //o
-// game.makeMove(2,0); //x
-
-let ctrl: Controller = new Controller(game); //will create the View
